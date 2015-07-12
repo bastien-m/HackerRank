d550eb93f5913d48ef01dd72ad1d746dd48efa47
bastien <method1990@gmail.com>
2015-07-12 21:04:32 +0200

    creation of the project with the solution for caesar cipher

---
+++ HackerRank/CaesarCipher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace HackerRank
+{
+    class CaesarCipher
+    {
+        public String MessageToEncode { get; set; }
+        public int ShiftingKey { get; set; }
+        public String EncodedMessage { get; set; }
+        private int numberOfCharacater = 26;
+
+        public CaesarCipher(String readableString, int shift)
+        {
+            this.MessageToEncode = readableString;
+            this.ShiftingKey = shift % 26;
+        }
+
+
+        public String Resolve()
+        {
+            StringBuilder EncodedMessageBuilder = new StringBuilder();
+            foreach (char c in this.MessageToEncode)
+            {
+                if (Char.IsLetter(c))
+                {
+                    EncodedMessageBuilder.Append(this.ShiftChar(c));
+                }
+                else
+                {
+                    EncodedMessageBuilder.Append(c);
+                }
+            }
+            this.EncodedMessage = EncodedMessageBuilder.ToString();
+            return this.EncodedMessage;
+        }
+
+        private char ShiftChar(char elementToShift)
+        {
+            int maxLowerCase = 122, maxUpperCase = 90;
+            int valueElementToShift = (int)elementToShift;
+            int valueElementShifted = valueElementToShift + this.ShiftingKey;
+            //We check if the shifted value is over Z or z (depend of case)
+            if ((valueElementToShift <= maxUpperCase && valueElementShifted > maxUpperCase) || 
+                (valueElementToShift > maxUpperCase && valueElementToShift <= maxLowerCase && valueElementShifted > maxLowerCase))
+            {
+                valueElementShifted-= this.numberOfCharacater;
+            }
+            return (char)(valueElementShifted);
+        }
+    }
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String firstMessage = String.Empty;
+            do
+            {
+                Console.WriteLine("Message a convertir : ");
+                firstMessage = Console.ReadLine();
+                Console.WriteLine("L'entier de decalage : ");
+                int shift = Int32.Parse(Console.ReadLine());
+                CaesarCipher cipher = new CaesarCipher(firstMessage, shift);
+                Console.WriteLine(cipher.Resolve());
+
+            } while (firstMessage.Equals("stop") == false);
+           
+            //Hacker rank main
+            Console.ReadLine();
+            String message = Console.ReadLine();
+            int shift01 = Int32.Parse(Console.ReadLine());
+            CaesarCipher cipher01 = new CaesarCipher(firstMessage, shift01);
+            Console.WriteLine(cipher01.Resolve());
+
+            //Break
+            Console.ReadLine();
+        }
+    }
+}
